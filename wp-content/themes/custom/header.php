fa67ec0fbe1a5ae558ec5348b1883386bb90b42b
Jordi Pamies <jordipamies2@gmail.com>
2015-08-07 14:23:02 +0100

    Added main hero

---
+++ wp-content/themes/custom/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php bloginfo( 'description' ); ?></title>
+	<link rel="stylesheet" href="<?php bloginfo(stylesheet_url) ?>">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:500,300,700,400' rel='stylesheet' type='text/css'>
+</head>
---
+++ wp-content/themes/custom/index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+<body>
+	<section class="header">
+		<header>
+			<div class="main-header">
+				<h1 class="title display3"><?php bloginfo('name') ?></h1>
+				<h2 class="subtitle"><?php bloginfo('description') ?></h2>
+			</div>
+		</header>
+	</section>
+	<section class="main">
+		
+	</section>
+</body>
