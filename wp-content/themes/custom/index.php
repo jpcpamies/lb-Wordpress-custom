f51a659b324bda6076557ff87ba3677e014c0b9e
Jordi Pamies <jordipamies2@gmail.com>
2015-08-07 21:26:14 +0100

    Adding banner-home

--- wp-content/themes/custom/index.php
+++ wp-content/themes/custom/index.php
@@ -1,70 +1,86 @@
 <?php get_header(); ?>
 <body>
-	<section class="Header">
+	<div class="Header">
 		<header>
 			<div class="Main-header">
-				<h1 class="Title "><?php bloginfo('name') ?></h1>
-				<h2 class="Subtitle"><?php bloginfo('description') ?></h2>
+				<h1 class="Title  display1"><?php bloginfo('name') ?></h1>
+				<h2 class="Subtitle  headline"><?php bloginfo('description') ?></h2>
 			</div>
 		</header>
-	</section>
-	<section class="Main">
-
-		<div class="Category Category-1">
-			<div class="Category__header">
-				<h3>Flores</h3>
-			</div>
-			<div class="Category__body">
-				<?php query_posts( 'order=Des&cat=2' ); ?>
+	</div>
+	<div class="Main  ModuleA">
+		<section class="Landing-banner  ModuleB">
+			<div class="Landing-banner__container">
+				<?php query_posts( 'cat=4' ); ?>
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<article>
-						<div class="Course-post">
-							<header class='Cours-post__header'>
-								<h3><?php the_title( ); ?></h3>
-							</header>
-							<figure class='Course-post__logo'>
-								<?php the_post_thumbnail( 'thumbnail' ); ?>
-							</figure>
-							<footer class='Course-post__link'>
-								<?php the_content( ); ?>
-							</footer>
-						</div>
-					</article>
+					<header class='Landing-banner__title  headline'>
+						<h3><?php the_title( ); ?></h3>
+					</header>
+					<div  class='Landing-banner__copy  body1'>
+						<?php the_content( ); ?>
+					</div>
 				<?php endwhile; ?>
 				<!-- post navigation -->
 				<?php else: ?>
 				<!-- no posts found -->
 				<?php endif; ?>
 			</div>
-		</div>
-
-		<div class="Category Category-2">
-			<div class="Category__header">
-				<h3>Frutas</h3>
+		</section>
+		<section class="Courses-feed">
+			<div class="Category Category-1  ModuleB">
+				<div class="Category__header">
+					<h3>Flores</h3>
+				</div>
+				<div class="Category__body">
+					<?php query_posts( 'order=Des&cat=2' ); ?>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						<article>
+							<div class="Course-post">
+								<header class='Cours-post__header  body1'>
+									<h3><?php the_title( ); ?></h3>
+								</header>
+								<figure class='Course-post__logo'>
+									<?php the_post_thumbnail( 'thumbnail' ); ?>
+								</figure>
+								<footer class='Course-post__link'>
+									<?php the_content( ); ?>
+								</footer>
+							</div>
+						</article>
+					<?php endwhile; ?>
+					<!-- post navigation -->
+					<?php else: ?>
+					<!-- no posts found -->
+					<?php endif; ?>
+				</div>
 			</div>
-			<div class="Category__body">
-				<?php query_posts( 'order=Des&cat=3' ); ?>
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-					<article>
-						<div class="Course-post">
-							<header class='Cours-post__header'>
-								<h3><?php the_title( ); ?></h3>
-							</header>
-							<figure class='Course-post__logo'>
-								<?php the_post_thumbnail( 'thumbnail' ); ?>
-							</figure>
-							<footer class='Course-post__link'>
-								<?php the_content( ); ?>
-							</footer>
-						</div>
-					</article>
-				<?php endwhile; ?>
-				<!-- post navigation -->
-				<?php else: ?>
-				<!-- no posts found -->
-				<?php endif; ?>
+			<div class="Category Category-2  ModuleB">
+				<div class="Category__header">
+					<h3>Frutas</h3>
+				</div>
+				<div class="Category__body">
+					<?php query_posts( 'order=Des&cat=3' ); ?>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+						<article>
+							<div class="Course-post">
+								<header class='Cours-post__header  body1'>
+									<h3><?php the_title( ); ?></h3>
+								</header>
+								<figure class='Course-post__logo'>
+									<?php the_post_thumbnail( 'thumbnail' ); ?>
+								</figure>
+								<footer class='Course-post__link'>
+									<?php the_content( ); ?>
+								</footer>
+							</div>
+						</article>
+					<?php endwhile; ?>
+					<!-- post navigation -->
+					<?php else: ?>
+					<!-- no posts found -->
+					<?php endif; ?>
+				</div>
 			</div>
-		</div>
-		
-	</section>
+		</section>
+	</div>
 </body>
